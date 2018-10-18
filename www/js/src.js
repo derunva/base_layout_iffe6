bbe7e526fbb8dc05925b2d9a6de776dc092fd166
derun <derun.va@gmail.com>
2018-10-18 20:07:17 +0300

    menu mobile

--- www/js/src.js
+++ www/js/src.js
@@ -1,1 +1,14 @@
 console.log(document, ' file')
+var menu_opener = document.getElementById('menu-opener');
+var menu = document.getElementById('menu');
+menu_opener.addEventListener('click', function(){
+  
+  if(menu_opener.classList.contains('active')){
+    menu_opener.classList.remove("active");
+    menu.classList.remove("active");
+  }else{
+    menu_opener.classList.add("active");
+    menu.classList.add("active");
+  }
+})
+console.log(menu_opener)
