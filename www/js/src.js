389739cc6a1e4c003d868bf9077ec90ecb1eac20
derun <derun.va@gmail.com>
2018-10-20 12:39:47 +0300

    tabs

--- www/js/src.js
+++ www/js/src.js
@@ -3,27 +3,33 @@ var menu_opener = document.getElementById('menu-opener');
 console.log(menu_opener)
 var menu = document.getElementById('menu');
 console.log(menu)
-menu_opener.addEventListener('click', function(){
-  console.log('click work')
-  console.log(menu_opener.classList.contains('active'))
-  if(menu_opener.classList.contains('active')){
-    console.log('triggered true')
-    menu_opener.classList.remove("active");
-    menu.classList.remove("active");
-  }else{
-    console.log('triggered false')
-    menu_opener.classList.add("active");
-    menu.classList.add("active");
+if(menu_opener){
+  menu_opener.addEventListener('click', function(){
+    console.log('click work')
+    console.log(menu_opener.classList.contains('active'))
+    if(menu_opener.classList.contains('active')){
+      console.log('triggered true')
+      menu_opener.classList.remove("active");
+      menu.classList.remove("active");
+    }else{
+      console.log('triggered false')
+      menu_opener.classList.add("active");
+      menu.classList.add("active");
+    }
+  })
+}
+  
+var tabNavItems = document.querySelectorAll('.tabs__nav [data-tab]')
+console.log(tabNavItems)
+tabNavItems.forEach(
+  function(item, index, arr){
+    console.log(item === arr[index])
+    console.log(item,index,arr)
+    item.addEventListener('click', function(event){
+      event.preventDefault();
+      console.log(event)
+      console.log(this)
+      console.log(this.dataset.tab)
+    })
   }
-})
-document.querySelector('css selector')
-document.querySelectorAll('css selector')
-jQuery('css selector')
-
-$('#menu-opener').on('click', function(){
-  if($('#menu-opener').is('.active')){
-    $('#menu-opener,#menu').removeClass('active')
-  }else{
-    $('#menu-opener,#menu').addClass('active')
-  }
-})
+)
